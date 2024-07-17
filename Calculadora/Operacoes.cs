e96bb70c37da3152394ebb9cb7f0f4c75950fe18
MichellSeidl <94243663+MichellSeidl@users.noreply.github.com>
2024-07-17 20:42:54 -0300

    "Add the case resolution files."

---
+++ Calculadora/Operacoes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Calculadora
+{
+    public class Operacoes
+    {
+        public long valorA {get;set;}
+        public char operador { get; set; }
+        public long valorB { get; set; }
+        public decimal resultado { get; set; }
+        public void ExibeOperacoes(Queue<Operacoes> filaOperacoes)
+        {
+            Console.WriteLine("Operações restantes na fila:");
+            foreach (Operacoes op in filaOperacoes)
+            {
+                Console.WriteLine("{0} {1} {2} ", op.valorA, op.operador, op.valorB);
+            }
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Operacoes> filaOperacoes = new Queue<Operacoes>();
+            filaOperacoes.Enqueue(new Operacoes { valorA = 2, valorB = 3, operador = '+' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 14, valorB = 8, operador = '-' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 5, valorB = 6, operador = '*' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 2147483647, valorB = 2, operador = '+' });
+            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' }); //Implemente o calculo de divisao
+
+            Calculadora calculadora = new Calculadora();
+
+            Stack<decimal> resultados = new Stack<decimal>();
+            
+            while (filaOperacoes.Count > 0)
+            {
+                Operacoes operacao = filaOperacoes.Peek();
+                operacao.ExibeOperacoes(filaOperacoes);
+                calculadora.calcular(operacao);
+                resultados.Push(operacao.resultado);
+                Console.WriteLine("\n{0} {1} {2} = {3}\n", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
+                filaOperacoes.Dequeue();
+            }
+
+            Stack<decimal> resultados2 = new Stack<decimal>(resultados.ToArray());
+
+            foreach (decimal resultado in resultados2)
+            {
+                Console.WriteLine(resultado);
+            }
+
+
+        }
+    }
+}
